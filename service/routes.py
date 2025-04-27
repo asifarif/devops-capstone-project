b80bdc1715dd6070b89708c5594b13771d18e937
asifarif <asif.arif@gmail.com>
2025-04-27 09:02:58 -0400

    updated routes.py file

--- service/routes.py
+++ service/routes.py
@@ -97,11 +97,11 @@ def get_accounts(account_id):
     Returns the account details or 404 if not found.
     """
     app.logger.info("Request to read an Account with id: %s", account_id)
-    
+
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-       
+
     return account.serialize(), status.HTTP_200_OK
 
 
